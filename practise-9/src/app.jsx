ed87d746c122115893d6f5efeaab0e88527b9efd
ZhongJinHacker <jiangzhongjin@hotmail.com>
2020-10-21 21:27:28 +0800

    practise-9 babel react support

---
+++ practise-9/src/app.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+class App extends React.Component{
+    constructor() {
+        super()
+    }
+    render() {
+        return <div>Hello World!!!!!</div>
+    }
+}
+export default App
