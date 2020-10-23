cd381804b42ab8b72129e69874a56ab99ee7036f
ZhongJinHacker <jiangzhongjin@hotmail.com>
2020-10-23 09:53:59 +0800

    practise-10 支持内联style css

---
+++ practise-10/src/app.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+class App extends React.Component {
+    constructor() {
+        super()
+    }
+    render() {
+        return <div style={{ background: 'red', width: '200px', height: '200px' }} >Hello World!!!!!</div>
+    }
+}
+export default App
